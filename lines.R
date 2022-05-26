046af79c38e3772bdfbe5301adbc2f0f7ced217f
bagsho <oaktas@gmail.com>
2022-05-26 20:43:29 +0300

    add lines from web

---
+++ lines.R
@@ -0,0 +1,20 @@
+7N
+https://athens.doublemap.com/map/text?route=76
+
+7A
+https://athens.doublemap.com/map/text?route=78
+
+Line 6
+https://athens.doublemap.com/map/text?route=57
+
+Line 5
+https://athens.doublemap.com/map/text?route=56
+
+Line 4
+https://athens.doublemap.com/map/text?route=55
+
+Line 3
+https://athens.doublemap.com/map/text?route=54
+
+Line 2
+https://athens.doublemap.com/map/text?route=53
